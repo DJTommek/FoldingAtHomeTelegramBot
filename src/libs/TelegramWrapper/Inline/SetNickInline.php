cdf18580db7f8be1dbefe9dc08075b767e3a9afb
DJTommek <tomas@palider.cz>
2020-04-05 01:48:02 +0200

    Added detailed info if used "set default" button on team or nick, which is already default

--- src/libs/TelegramWrapper/Inline/SetNickInline.php
+++ src/libs/TelegramWrapper/Inline/SetNickInline.php
@@ -14,6 +14,11 @@ class SetNickInline extends Inline
 		$foldingUserId = intval($this->params[0]);
 		$foldingUserName = base64_decode($this->params[1]);
 
+		if ($user->getFoldingId() === $foldingUserId) {
+			$this->flash(sprintf('%s Nick "%s" is already your default, you can use command /stats without parameter.', Icons::INFO, $user->getFoldingName()), true);
+			return;
+		}
+
 		Debugger::log($this->params);
 		$user->update(null, $foldingUserId, $foldingUserName);
 		$msg = sprintf('%s Nick "%s" was set as default, now you can use command /stats without parameter.', Icons::SUCCESS, htmlentities($foldingUserName));
--- src/libs/TelegramWrapper/Inline/SetTeamInline.php
+++ src/libs/TelegramWrapper/Inline/SetTeamInline.php
@@ -13,6 +13,11 @@ class SetTeamInline extends Inline
 		$foldingTeamId = intval($this->params[0]);
 		$foldingTeamName = base64_decode($this->params[1]);
 
+		if ($user->getFoldingTeamId() === $foldingTeamId) {
+			$this->flash(sprintf('%s Team "%s" is already your default, you can use command /team without parameter.', Icons::INFO, $user->getFoldingTeamName()), true);
+			return;
+		}
+
 		$user->updateTeam($foldingTeamId, $foldingTeamName);
 		$msg = sprintf('%s Team "%s" was set as default, now you can use command /team without parameter.', Icons::SUCCESS, htmlentities($foldingTeamName));
 		$this->flash($msg, true);
