8f024644f2c653b8acaf885e505a257b78989a00
DJTommek <tomas@palider.cz>
2020-04-05 01:55:38 +0200

    Updated /help message

--- src/libs/TelegramWrapper/Command/HelpCommand.php
+++ src/libs/TelegramWrapper/Command/HelpCommand.php
@@ -18,28 +18,14 @@ class HelpCommand extends Command
 		$text .= sprintf('Simple bot which help you to get statistics from <a href="%s">%s</a> website here into Telegram.', Folding::STATS_URL, Folding::STATS_URL) . PHP_EOL;
 		$text .= PHP_EOL;
 		$text .= sprintf(Icons::USER . ' <b>User commands</b>:') . PHP_EOL;
-
-		$text .= sprintf(' /stats - load your personal statistics');
-		if ($user->getFoldingName()) {
-			$text .= sprintf(' (currently set to user <a href="%s">%s</a>)', $user->getUrl(), $user->getFoldingName());
-		}
-		$text .= PHP_EOL;
-
+		$text .= sprintf(' /stats - load your personal statistics. Currently set to user <a href="%s">%s</a>', $user->getUrl(), htmlentities($user->getFoldingName())) . PHP_EOL;
 		$text .= sprintf(' /stats &lt;nick, ID or URL&gt; - load specific user statistics') . PHP_EOL;
-//		$text .= sprintf(' /setNick &lt;nick or ID or URL&gt; - set default nick to different than your Telegram username.') . PHP_EOL;
 		$text .= PHP_EOL;
 		$text .= sprintf(Icons::TEAM . ' <b>Team commands</b>:') . PHP_EOL;
-
-		$text .= sprintf(' /team - load your team statistics.');
-		if ($user->getFoldingTeamId()) {
-			$text .= sprintf(' (currently set to team <a href="%s">%s</a>)', (Folding::getTeamUrl($user->getFoldingTeamId())), $user->getFoldingTeamName());
-		}
-		$text .= PHP_EOL;
-
+		$text .= sprintf(' /team - load your team statistics. Currently set to team <a href="%s">%s</a>', (Folding::getTeamUrl($user->getFoldingTeamId())), htmlentities($user->getFoldingTeamName())) . PHP_EOL;
 		$text .= sprintf(' /team &lt;team ID or URL&gt; - load specific team statistics') . PHP_EOL;
-//		$text .= sprintf(' /setTeam &lt;ID or URL&gt; - set default team') . PHP_EOL;
 		$text .= PHP_EOL;
-		$text .= sprintf('%s <b>Warning</b>: Website API is often very slow so be patient. Bot has automatic timeout set to %d seconds, then it will reply with sorry message.',
+		$text .= sprintf('%s <b>Warning</b>: Website API is often very slow so be patient. Bot has automatic timeout set to %d seconds, then it will reply with sorry message. In few seconds try again and response should be quick.',
 				Icons::WARNING, FOLDING_STATS_TIMEOUT) . PHP_EOL;
 
 		$this->reply($text);
