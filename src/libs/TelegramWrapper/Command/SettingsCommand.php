8beadef6f0da3329119f46b388bbd07b15ccc54f
DJTommek <tomas@palider.cz>
2020-04-23 16:45:39 +0200

    #3 User timezone: updated messages and buttons

--- src/libs/TelegramWrapper/Command/SettingsCommand.php
+++ src/libs/TelegramWrapper/Command/SettingsCommand.php
@@ -13,14 +13,19 @@ class SettingsCommand extends Command
 {
 	public function __construct($update, $tgLog, $loop, \User $user) {
 		parent::__construct($update, $tgLog, $loop, $user);
+
+		$userTimezone = $this->user->getTimezone();
+		$text = sprintf('<b>Settings</b>') . PHP_EOL;
+		$text .= sprintf('Choose one of the settings via buttons below:') . PHP_EOL;
+
 		$replyMarkup = new Markup();
 		$replyMarkup->inline_keyboard[] = [
 			[
-				'text' => sprintf('Settings'),
-				'callback_data' => sprintf(self::CMD_SETTINGS),
+				'text' => sprintf('%s Timezone: %s', Icons::CLOCK, $userTimezone->getName()),
+				'callback_data' => sprintf(Command::CMD_SETTINGS_TIMEZONE),
 			],
 		];
 
-		$this->reply('Just button here', $replyMarkup);
+		$this->reply($text, $replyMarkup);
 	}
 }
--- src/libs/TelegramWrapper/Inline/SettingsInline.php
+++ src/libs/TelegramWrapper/Inline/SettingsInline.php
@@ -13,22 +13,21 @@ class SettingsInline extends Inline
 	public function __construct($update, $tgLog, $loop, \User $user) {
 		parent::__construct($update, $tgLog, $loop, $user);
 
-		$replyMarkup = new Markup();
 
 		$userTimezone = $this->user->getTimezone();
 		$text = sprintf('<b>Settings</b>') . PHP_EOL;
-		$text .= sprintf('Choose which settings via buttons below:') . PHP_EOL;
+		$text .= sprintf('Choose one of the settings via buttons below:') . PHP_EOL;
 
+		$replyMarkup = new Markup();
 		$replyMarkup->inline_keyboard = [
 			[ // row of buttons
 				[ // button
-					'text' => sprintf('Timezone: %s', $userTimezone->getName()),
+					'text' => sprintf('%s Timezone: %s', Icons::CLOCK, $userTimezone->getName()),
 					'callback_data' => sprintf(Command::CMD_SETTINGS_TIMEZONE),
 				],
 			],
 		];
 
 		$this->replyButton($text, $replyMarkup);
-		$this->flash('Just ACK flash');
 	}
 }
